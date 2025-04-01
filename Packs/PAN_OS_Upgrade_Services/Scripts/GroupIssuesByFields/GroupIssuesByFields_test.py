5d18fb32bc99548db6ff6d3cb56d8a3b6806d75d
Robin <167366979+allrob23@users.noreply.github.com>
2025-04-01 15:12:54 -0300

    feat: replace bare open() with with statements for better file handling

--- Packs/PAN_OS_Upgrade_Services/Scripts/GroupIssuesByFields/GroupIssuesByFields_test.py
+++ Packs/PAN_OS_Upgrade_Services/Scripts/GroupIssuesByFields/GroupIssuesByFields_test.py
@@ -6,12 +6,13 @@ TEST_LOCATION_FROM_ROOT = "Packs/PAN_OS_Upgrade_Services/Scripts/GroupIssuesByFi
 def load_json_from_test_file(file_name):
     """Given a JSON file, returns the contents"""
     try:
-        data = json.load(open(file_name))
+        with open(file_name) as f:
+            data = json.load(f)
     except FileNotFoundError:
         # Handle test being run outside of test directory (i.e from content root dir)
-        data = json.load(
-            open(TEST_LOCATION_FROM_ROOT + file_name))
-
+        with open(TEST_LOCATION_FROM_ROOT + file_name) as f:
+            data = json.load(f)
+    
     return data
 
 
